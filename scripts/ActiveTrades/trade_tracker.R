7ec39358d9bc0d6395962fdfdada4c4cd066195d
tmolone1 <tpmoloney@gmail.com>
2021-03-12 12:49:58 -0700

    inactivated a couple closed trades

--- scripts/ActiveTrades/trade_tracker.R
+++ scripts/ActiveTrades/trade_tracker.R
@@ -5,7 +5,7 @@ i<-6
 symbols<-c("LOW", "FMC", "DOCU","XRT","AAL", "NEE", "QQQ", "QRVO", "SPY", "IWM")
 strategy<-c("long_call", "front_spread_with_calls","bull_put_spread","short_call_spread","long_call_spread", "bull_put_spread", "short_call_spread", "diagonal_call_spread", "skip_strike_call_butterfly", "back_spread_with_calls")
 Active_Trades<-tibble(symbols, strategy, basis=0, profit=0)
-for (i in 2:10) {
+for (i in c(2:6,8:9)) {
 symb<-Active_Trades[i,]$symbols
 strat<-Active_Trades[i,]$strategy
 writeLines(c(symb,strat))
